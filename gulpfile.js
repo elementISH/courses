2f9841e7dbd5227def61d176ea25bfa96601ee05
ISH <70657630+elementISH@users.noreply.github.com>
2021-09-19 18:22:48 +0200

    updated gulp, index and fixed some styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'),
+var gulp = require('gulp')
     concat = require('gulp-concat'),
     prefix = require('gulp-autoprefixer'),
     sass = require('gulp-sass')(require('sass')),
