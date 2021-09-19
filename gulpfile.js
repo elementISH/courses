d8fc28aaabd48fd57b5a233c44b428626f1f5dd4
som3a12 <70657630+som3a12@users.noreply.github.com>
2021-09-19 18:25:42 +0200

    main stable version of code

--- gulpfile.js
+++ gulpfile.js
@@ -7,77 +7,10 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     minify = require('gulp-minify');
 
-gulp.task('SassCompile', function () {
+gulp.task('SassCompileLtr', function () {
 
-    return gulp.src(['content/scss/main-ltr.scss', 'content/scss/main-rtl.scss'])
+    return gulp.src(['content/scss/main-ltr.scss'])
         .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', function (err) {
-            console.log(`-----------------------------------------------------------------`);
-            console.log(err.message);
-            console.log(`-----------------------------------------------------------------`);
-            notifier.notify(
-                {
-                    title: 'Sass Error Compiling',
-                    message: `Error in File : ${err.relativePath} \nError in Line : ${err.line} , Column : ${err.column} `,
-                    sound: false,
-                    wait: false,
-                    timeout: 1
-                },
-                function (err, response) {
-                    // Response is response from notification
-                }
-                
-            );
-            this.emit('end');
-        }))
-        .pipe(concat("all.css"))
-        .pipe(sourcemaps.init())
-        .pipe(prefix('last 2 versions'))
-        .pipe(minify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("dist"))
-});
-
-gulp.task('vendorsCss', function(){
-    return gulp.src([
-        'content/css/vendors/icomoon/style.css',
-        'content/css/vendors/bootstrap.min.css',
-        'content/css/vendors/swiper.min.css'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', function (err) {
-        console.log(`-----------------------------------------------------------------`);
-        console.log(err.message);
-        console.log(`-----------------------------------------------------------------`);
-        notifier.notify(
-            {
-                title: 'Sass Error Compiling',
-                message: `Error in File : ${err.relativePath} \nError in Line : ${err.line} , Column : ${err.column} `,
-                sound: false,
-                wait: false,
-                timeout: 1
-            },
-            function (err, response) {
-                // Response is response from notification
-            }
-            
-        );
-        this.emit('end');
-    }))
-    .pipe(concat("vendors.css"))
-    .pipe(sourcemaps.init())
-    .pipe(prefix('last 2 versions'))
-    .pipe(minify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest("content/css/vendors"))
-})
-
-gulp.task('compressedLtr', function(){
-    return gulp.src([
-        'content/css/vendors/vendors.css',
-        'content/scss/main-ltr.scss'
-    ])
-    .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', function (err) {
             console.log(`-----------------------------------------------------------------`);
             console.log(err.message);
@@ -103,14 +36,12 @@ gulp.task('compressedLtr', function(){
         .pipe(minify())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest("content/css"))
-})
+});
 
-gulp.task('compressedRtl', function(){
-    return gulp.src([
-        'content/css/vendors/vendors.css',
-        'content/scss/main-rtl.scss'
-    ])
-    .pipe(sourcemaps.init())
+gulp.task('SassCompileRtl', function () {
+
+    return gulp.src(['content/scss/main-rtl.scss'])
+        .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', function (err) {
             console.log(`-----------------------------------------------------------------`);
             console.log(err.message);
@@ -136,8 +67,51 @@ gulp.task('compressedRtl', function(){
         .pipe(minify())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest("content/css"))
+});
+
+gulp.task('vendorsCss', function(){
+    return gulp.src([
+        'content/css/vendors/icomoon/style.css',
+        'content/css/vendors/bootstrap.min.css',
+        'content/css/vendors/swiper.min.css'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat("vendors.css"))
+    .pipe(sourcemaps.init())
+    .pipe(prefix('last 2 versions'))
+    .pipe(minify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("content/css/vendors"))
+})
+
+gulp.task('compressedLtr', function(){
+    return gulp.src([
+        'content/css/vendors/vendors.css',
+        'content/css/main-ltr.css'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat("main-ltr.css"))
+    .pipe(sourcemaps.init())
+    .pipe(prefix('last 2 versions'))
+    .pipe(minify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("content/css"))
+})
+
+gulp.task('compressedRtl', function(){
+    return gulp.src([
+        'content/css/vendors/vendors.css',
+        'content/css/main-rtl.css'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat("main-rtl.css"))
+    .pipe(sourcemaps.init())
+    .pipe(prefix('last 2 versions'))
+    .pipe(minify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest("content/css"))
 })
 
 gulp.task('watch', function () {
-    gulp.watch(['content/scss/*/*.scss', 'content/scss/*.scss'], gulp.series(['SassCompile', 'vendorsCss', 'compressedLtr', 'compressedRtl']));
+    gulp.watch(['content/scss/*/*.scss', 'content/scss/*.scss'], gulp.series(['SassCompileLtr', 'SassCompileRtl', 'vendorsCss', 'compressedLtr', 'compressedRtl']));
 });
