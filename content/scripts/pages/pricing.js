1b6d45d0cee6a4aa3a9655efc76f2f4e82511699
som3a12 <70657630+som3a12@users.noreply.github.com>
2021-09-22 12:42:40 +0200

    s

--- content/scripts/pages/pricing.js
+++ content/scripts/pages/pricing.js
@@ -4,12 +4,14 @@ let animationDiv = $(".animation-div");
 let yearlyBtn = $(".btn-yearly");
 let monthlyBtn =  $(".btn-monthly");
 let newPrice;
+let clickedBtn;
 
 function changeToYearly() {
+    if(clickedBtn == "yearlyBtn") return;
     animationDiv.css('display', 'block');
     monthlyBtn.css('background', '#fff');
     monthlyBtn.css('color', '#002147');
-    animationDiv.animate({right: '365px'}, 500).promise().done(function(res){
+    animationDiv.animate({right: '-=140px'}, 500).promise().done(function(res){
         yearlyBtn.css('background', '#00b2b2');
         yearlyBtn.css('color', '#fff');
         animationDiv.css('display', 'none');
@@ -18,13 +20,15 @@ function changeToYearly() {
             $(price).html(newPrice.toFixed(2))
             })
         })
+    clickedBtn = "yearlyBtn";
 }
 
 function changeToMonthly() {
+    if(clickedBtn == "monthlyBtn") return;
     animationDiv.css('display', 'block');
     yearlyBtn.css('background', '#fff');
     yearlyBtn.css('color', '#002147');
-    animationDiv.animate({right: '500px'}, 500).promise().done(function(res){
+    animationDiv.animate({right: '+=140px'}, 500).promise().done(function(res){
         monthlyBtn.css('background', '#00b2b2');
         monthlyBtn.css('color', '#fff');
         animationDiv.css('display', 'none');
@@ -33,6 +37,7 @@ function changeToMonthly() {
         $(price).html(newPrice.toFixed(2))
         })
     })
+    clickedBtn = "monthlyBtn";
 }
 
 $(yearlyBtn).on('click', changeToYearly);
